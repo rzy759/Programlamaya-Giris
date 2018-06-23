137d13f86da508d37be615409ef42e417781185d
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-06-23 08:03:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace döngü_ile_ardışık_sayı_bastıran_kod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
