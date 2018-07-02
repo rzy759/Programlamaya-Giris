2e5681ec2eafce788337ac65f80de5592d4d71f5
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-02 08:10:24 +0300

    Add files via upload

---
+++ Ekrandan öğrenci numarası okuyan kod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen öğrenci numaranızı giriniz: ");
+            int numara=int.Parse(Console.ReadLine());
+            Console.WriteLine("öğrenci no: "+numara);
+
+            Console.ReadLine();
+        }
+    }
+}
