f0f1a48df169067899c5f551e6525a0b07f3bc09
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-02 08:04:49 +0300

    Add files via upload

---
+++ ekrandan isim okuyan kod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen isminizi giriniz: ");
+            string isim= Console.ReadLine();
+            Console.WriteLine("merhaba "+isim);
+
+            Console.ReadLine();
+        }
+    }
+}
