3a69b9f92cd1851aa1f61cc61eb142cbee5af9ed
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-05 22:59:12 +0300

    Add files via upload

---
+++ Obeb bulan kod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static int Ebob(int a,int b)
+        {
+            if (b == 0)
+                return a;
+            else
+                return Ebob(b,a % b);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sayi1: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("sayi2: ");
+            int b = int.Parse(Console.ReadLine());
+            int sonuc=Ebob(a, b);
+            Console.WriteLine("Obeb: "+ sonuc);
+            Console.ReadLine();
+        }
+    }
+}
