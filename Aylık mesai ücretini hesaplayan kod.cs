e7273546a7d2c56f598592b5a5885f826d6b1602
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-05 22:43:22 +0300

    Add files via upload

---
+++ Aylık mesai ücretini hesaplayan kod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Saat sayısını giriniz: ");
+            int saat = int.Parse(Console.ReadLine());
+
+            int ücret = 0;
+            if (saat <= 10)
+                ücret = saat * 1;
+            else if (saat <= 20)
+                ücret = saat * 2;
+            else if (saat <= 40)
+                ücret = saat * 3;
+            else if (saat <= 100)
+                ücret = saat * 4;
+            else
+                ücret = saat * 5;
+
+            
+            Console.WriteLine("Mesai Ücreti: "+ücret +"tl");
+            Console.ReadLine();
+        }
+    }
+}
