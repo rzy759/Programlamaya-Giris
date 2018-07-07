fde66ce63e5096ef2394bebf06b8ae0b6e540e65
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-07 07:59:28 +0300

    Add files via upload

---
+++ Verilen sayıların Ramanujan sayısı olup olmadığını bulan kod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("4 tane sayı giriniz");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+         
+            if (a * a * a + b * b * b == c * c * c + d * d * d )            
+                Console.WriteLine(  a+ "³ + " + b+"³ =" + c+"³ + "+d+"³ \n"+
+                    "verilen sayılar Ramanujan sayısıdır");            
+            else
+                Console.WriteLine(a + "³ + " + b + "³ =! " + c + "³ + " + d + "³ \n" + 
+                    "verilen sayılar Ramanujan sayısı değildir");
+            Console.ReadLine();
+        }
+    }
+}
