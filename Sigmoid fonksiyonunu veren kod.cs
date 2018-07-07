ade02931b97c98993e888b173361c4410ad96972
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-07 07:24:40 +0300

    Add files via upload

---
+++ Sigmoid fonksiyonunu veren kod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı giriniz: ");
+            double u = double.Parse(Console.ReadLine());            
+            
+            double a = Math.Exp(-u);
+            double sigmoid = 1 / (1 + a);
+
+            Console.WriteLine("sigmoid= "+sigmoid);
+            Console.ReadLine();
+        }
+    }
+}
