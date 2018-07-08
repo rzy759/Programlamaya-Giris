d2673ada5843aeda3bd52de9cfdfb3048fb9de42
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-08 16:30:14 +0300

    Add files via upload

---
+++ Binary bir sayıyı 10'luk tabana çevirme.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("bir sayı giriniz: ");
+            string sayı = Console.ReadLine();
+            int uzunluk=sayı.Length;
+            int sayı1=int.Parse(sayı);
+            double toplam = 0;
+
+            int[] sayılar=new int[uzunluk];
+            int i= 0;
+            //sayıyı diziye aktardık
+            while (sayı1 > 0)
+            {                
+                sayılar[i]=  sayı1 % 10;                                
+                sayı1 = sayı1 / 10;
+                i++;
+            }
+
+            for (i = 0; i < uzunluk; i++)
+            {
+               toplam=toplam+ (Math.Pow(2, i))*(sayılar[i]);
+            }       
+                              
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+        }
+    }
+}
