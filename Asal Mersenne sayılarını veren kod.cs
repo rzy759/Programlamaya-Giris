9d93f3227b5e88db65777cc630ebbf7b4dfd2777
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-09 07:33:43 +0300

    Add files via upload

---
+++ Asal Mersenne sayılarını veren kod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mers=0;
+            bool durum;
+
+            for (int i = 2; i < 50; i++)
+            {
+                durum = true;
+                for (int j =2; j < i; j++)
+                {
+                    if (i%j==0)
+                    {
+                        durum = false;
+                        break;
+                    }
+
+                }
+                
+                if (durum==true)
+                {
+                    mers = Math.Pow(2, i) - 1;
+                    Console.WriteLine(mers);                   
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
