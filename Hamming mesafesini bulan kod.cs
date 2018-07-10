f2d096a577a99b7808a2d63994795770f496fc5b
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-10 07:51:56 +0300

    Add files via upload

---
+++ Hamming mesafesini bulan kod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Console.WriteLine("iki dizgi giriniz: ");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int sayaç = 0;
+            char[] akarakterler = a.ToCharArray();
+            char[] bkarakterler = b.ToCharArray();
+            if (a.Length==b.Length)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    for (int j = 0; j <=i ; j++)
+                    {
+                        if (a[i] != b[i])
+                    {
+                        sayaç++;
+                        break;
+                    }                   
+                    }              
+                }
+            Console.WriteLine(sayaç);
+            }
+            else
+                Console.WriteLine("dizgi uzunlukları birbirine eşit değil ");           
+            
+            Console.ReadLine();
+        }
+    }
+}
