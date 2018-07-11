bfe947775a182d366263c4bb09f22872f98ecd32
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-11 07:41:11 +0300

    Add files via upload

---
+++ Toplama işlemi kullanarak çarpma işlemi yapan kod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iki sayı giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int sonuc = 0;
+
+            if (b < 0)
+            {
+                for (int i = b; i < 0; i++)           
+                sonuc -= a;   
+            }
+            else if (a<0)
+            {
+                for (int i = a; i < 0; i++)
+                sonuc -= b;
+                
+            }
+            else if (a < 0 && b < 0)
+            {
+                for (int i = 0; i < b; i++)
+                sonuc += a;
+            }
+            else
+            {
+                for (int i = 0; i < b; i++)            
+                sonuc += a;
+            }
+           
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+        }
+    }
+}
