b6ca074bcaca934bb2b3dca670eabde86cb13ff8
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-12 19:25:42 +0300

    Add files via upload

---
+++ Mutlak değer döndüren kod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int topla = 0;
+            int[] sayılar = new int[] { 7, 3, -2, 5, 0, 20 };
+
+            for (int i = 0; i < 6; i++)
+            {
+             sayılar[i]= Math.Abs(sayılar[i]);
+            }
+
+            for (int i = 0; i < 6; i=i+2)
+            {
+               topla= topla+sayılar[i] + sayılar[i + 1];
+            }
+
+            Console.WriteLine(topla);
+            Console.ReadLine();
+        
+        }
+    }
+}
