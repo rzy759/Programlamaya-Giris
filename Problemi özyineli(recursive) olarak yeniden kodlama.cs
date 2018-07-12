9676b5ecf0e46e50b6a906bae7fb492c4b6e9cb2
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-12 19:00:01 +0300

    Add files via upload

---
+++ Problemi özyineli(recursive) olarak yeniden kodlama.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp57
+{
+    class Program
+    {
+        static int Carpma(int a,int b)
+        {
+            int toplam = 0;
+            for (int i = 0; i < b; i++)
+            {
+                toplam += a;
+            }            
+            return toplam;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iki sayı giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int sonuc = Carpma(a, b);            
+           
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+        }
+    }
+}
