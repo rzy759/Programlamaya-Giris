30a4b58cb68d10e093e0f92fb904ce464248a2af
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-13 07:56:03 +0300

    Add files via upload

---
+++ Dizideki sayıların aritmetik ortalamasını döndüren kod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dizi kaç elemanlı: ");
+            int a = int.Parse(Console.ReadLine());
+            double[] sayılar = new double[a];
+            Console.WriteLine("sayıları giriniz: ");
+            for (int i = 0; i < a; i++)
+            {
+                sayılar[i] = double.Parse(Console.ReadLine());
+            }
+
+            double ort = sayılar.Sum() / a;
+            Console.WriteLine("ortalaması: "+ort);
+            Console.ReadLine();           
+        }
+    }
+}
