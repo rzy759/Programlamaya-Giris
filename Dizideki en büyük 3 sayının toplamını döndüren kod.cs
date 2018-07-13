dc866e781b3c2bea79d280347af341523a2b00b4
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-13 07:23:30 +0300

    Add files via upload

---
+++ Dizideki en büyük 3 sayının toplamını döndüren kod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dizi kaç elemanlı: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("sayıları giriniz: ");
+            int[] sayılar = new int[a];
+
+            for (int i = 0; i < a; i++)
+            {
+                sayılar[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(sayılar);
+            Array.Reverse(sayılar);
+
+            Console.WriteLine("\nbüyükten küçüğe sıralanmış hali:");
+            foreach (int i in sayılar)
+            {
+                Console.WriteLine(i);
+            }
+
+            int toplam = sayılar[0] + sayılar[1] + sayılar[2];
+            Console.WriteLine("\ntoplam: "+toplam);
+            Console.ReadLine();
+        }
+    }
+}
