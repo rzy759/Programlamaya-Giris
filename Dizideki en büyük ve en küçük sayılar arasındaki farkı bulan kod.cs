53a1238fcdb98126ee1033cc04ca7eccd5eb0b33
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-13 07:37:13 +0300

    Add files via upload

---
+++ Dizideki en büyük ve en küçük sayılar arasındaki farkı bulan kod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dizi kaç elemanlı: ");
+            int a = int.Parse(Console.ReadLine());
+            int[] sayılar = new int[a];
+
+            Console.WriteLine("sayıları giriniz: ");
+            for (int i = 0; i < a; i++)
+            {
+                sayılar[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nküçükten büyüğe sıralanmış hali: ");
+            Array.Sort(sayılar);
+            foreach (int i in sayılar)
+            {
+                Console.WriteLine(i);
+            }
+        
+            
+            int fark = sayılar[a-1] - sayılar[0];
+            Console.WriteLine("fark: "+fark);
+            Console.ReadLine();
+        }
+    }
+}
