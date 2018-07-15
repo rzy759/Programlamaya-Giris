dc03c31d5de37dd67e77f33d4a49970680fab486
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-15 18:51:20 +0300

    Add files via upload

---
+++ Dizideki sayıların geometrik ortalamasını döndüren kod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dizi kaç elemanlı: ");
+            int a = int.Parse(Console.ReadLine());
+            double çarpım = 1;
+            int[] sayılar = new int[a];
+            
+            Console.WriteLine("sayıları giriniz: ");
+            for (int i = 0; i < a; i++)
+            {
+                sayılar[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < a; i++)
+            {
+                çarpım = çarpım * sayılar[i];                   
+            }
+            double üs = 1 / (double)a;
+            
+            Console.WriteLine("geometrik ortalaması: "+Math.Pow(çarpım, üs));
+            Console.ReadLine();
+        }
+    }
+}
