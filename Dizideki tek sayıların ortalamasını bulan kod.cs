bb4de5717015bf824492ec5d596ef0555b8f7fc5
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-15 19:31:05 +0300

    Add files via upload

---
+++ Dizideki tek sayıların ortalamasını bulan kod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dizi kaç elemanlı: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("sayıları giriniz: ");
+            int[] sayılar = new int[a];
+            int toplam = 0;
+            int sayaç=0;
+            for (int i = 0; i < a; i++)
+            {
+                sayılar[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < a; i++)
+            {
+                if(sayılar[i] % 2 != 0)
+                {
+                    toplam += sayılar[i];
+                    sayaç++;
+                }
+            }
+            double ort = toplam / (double)sayaç;
+            Console.WriteLine("ortalaması: "+ort);
+            Console.ReadLine();        
+
+        
+        }
+    }
+}
