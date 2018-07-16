1d67dc6f57bf651bb9387df1f19ed69a144ccd34
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-16 07:13:10 +0300

    Add files via upload

---
+++ Dizideki çift sayıların en büyüğü ile en küçüğün ortalamasını bulma.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dizi kaç elemanlı: ");
+            int a = int.Parse(Console.ReadLine());
+
+            int b = 0;
+            Console.WriteLine("sayıları giriniz: ");
+            List<int> sayılar = new List<int>();
+           
+            for (int i = 0; i < a; i++)
+            {
+                b=int.Parse(Console.ReadLine());
+                if (b % 2 == 0)
+                    sayılar.Add(b);
+            }
+
+            int ort = (sayılar.Max() + sayılar.Min()) / 2;
+            Console.WriteLine("çift sayıların en büyüğü ile en küçüğün ortalaması: "+ort);          
+            Console.ReadLine();
+        }
+    }
+}
