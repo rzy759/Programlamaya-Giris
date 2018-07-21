4ad770c15b75794ed8da0c12c44fb79f6ba86993
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-21 07:06:54 +0300

    Add files via upload

---
+++ Matrisi sabit ile çarpma.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("matrisin satır ve sutununu giriniz: ");
+            
+            int sa = int.Parse(Console.ReadLine());
+            int su = int.Parse(Console.ReadLine());
+            int[,] A = new int[sa, su];
+
+            Console.WriteLine("matris elemanlarını giriniz: ");
+            for (int i = 0; i < sa; i++)
+            {
+                for (int j = 0; j < su; j++)
+                {
+                    A[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("hangi sayı ile çarpılacak: ");
+            int sayı = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sa; i++)
+            {
+                for (int j = 0; j < su; j++)
+                {
+                    A[i, j] = A[i, j] * sayı;
+                }
+
+            }
+            Console.WriteLine("sonuç: ");
+            for (int i = 0; i < sa; i++)
+            {
+                for (int j = 0; j < su; j++)
+                {
+                    Console.Write(A[i,j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
