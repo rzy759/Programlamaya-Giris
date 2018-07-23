55ceac13ee3acde3ce6221e228c42d90bbf22fc1
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-23 09:27:03 +0300

    Add files via upload

---
+++ Dairenin alanını hesaplama.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dairenin yarıçapını giriniz: ");
+            int r = int.Parse(Console.ReadLine());
+
+            double alan = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine("dairenin alanı: "+alan);
+            Console.ReadLine();
+        }
+    }
+}
