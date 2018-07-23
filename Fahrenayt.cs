11e03b3b15747d9d690f9a6f5c8cbbff2d90657f
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-23 09:05:38 +0300

    Add files via upload

---
+++ Fahrenayt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("celcius değerini giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+
+            double fahr=(a*1.8)+32;
+
+            Console.WriteLine("Fahrenayt: "+fahr+ "°F");
+            Console.ReadLine();
+        }
+    }
+}
