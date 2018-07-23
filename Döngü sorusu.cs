89d5bc35fdc6b6d7cdc4b1793f07ed0d6e931114
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-23 08:52:39 +0300

    Add files via upload

---
+++ Döngü sorusu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iki farklı sayı giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int toplam = 0;
+            int sayac = 0;
+            Console.WriteLine("tek sayılar: ");
+            if (a > b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;                
+            }
+            for (int i = a; i < b; i++)
+                {
+                    if (i%2!=0)
+                    {
+                        Console.WriteLine(i);
+                        toplam += i;
+                        sayac++;
+                    }
+                    else                
+                        sayac = 1;
+                
+                }
+
+            int ort = toplam / sayac;
+            Console.WriteLine("ortalama: "+ort);
+            Console.ReadLine();
+        }
+    }
+}
