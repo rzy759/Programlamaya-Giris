cff16a377450fe343ece58cf666e538173110935
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-24 07:33:37 +0300

    Add files via upload

---
+++ Girilen sayıya kadar olan sayıların karelerini hesaplama.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp76
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 den büyük bir sayı giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = 0;
+
+            for (int i = 1; i <= a; i++)
+            {
+                b +=  (i * i);               
+
+            }
+            Console.WriteLine("Kareler toplamı ="+b);
+            Console.ReadLine();
+            
+        }
+    }
+}
