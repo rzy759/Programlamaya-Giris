e7ae3d574b609f2c73495b2474658cc40ca8d9fc
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-24 07:46:52 +0300

    Add files via upload

---
+++ Bir metindeki kelime sayısını bulma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir metin giriniz: ");
+            string a = Console.ReadLine();
+            string[] D = a.Split(' ');
+            int sayac = 0;
+            foreach (var i in D)
+            {
+                sayac++;
+            }
+            Console.WriteLine(sayac);
+            Console.ReadLine();
+        }
+    }
+}
