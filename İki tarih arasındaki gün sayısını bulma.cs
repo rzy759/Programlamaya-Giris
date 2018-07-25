44b2b5a483bd39a776710b2dd8907acb485696b1
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-25 07:41:48 +0300

    Add files via upload

---
+++ İki tarih arasındaki gün sayısını bulma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iki tane tarih giriniz: ");
+            DateTime a = DateTime.Parse(Console.ReadLine());
+            DateTime b = DateTime.Parse(Console.ReadLine());
+
+            TimeSpan c = a - b;
+            
+            Console.WriteLine("gün sayısı: "+c.TotalDays);
+            Console.ReadLine();             
+                       
+        }
+    }
+}
