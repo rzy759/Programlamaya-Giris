683fb04c8eb4f52ab8b827ef3bee2aacf3dd4519
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-26 14:24:31 +0300

    Add files via upload

---
+++ İsmin baş harfini yazdırma.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] isim = new string[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("ismi girin: ");
+                isim[i] = Console.ReadLine();
+            }
+              
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(isim[i][0]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
