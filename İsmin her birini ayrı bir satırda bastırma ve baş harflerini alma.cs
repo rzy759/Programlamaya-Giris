062c12148f870ffaefd61ecbd1ccc7d29bf71aed
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-27 07:20:40 +0300

    Add files via upload

---
+++ İsmin her birini ayrı bir satırda bastırma ve baş harflerini alma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ismi girin: ");
+            string isim = Console.ReadLine();
+            string[] ayrı=isim.Split(' ');
+            foreach (var item in ayrı)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in ayrı)
+            {
+                Console.Write(item[0]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
